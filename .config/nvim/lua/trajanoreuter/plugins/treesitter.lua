5b790de55418f75847fa809c8718c08d730123c2
Trajano Reuter <trajanoreuter@gmail.com>
2024-05-02 16:57:27 -0300

    fix: warning

--- .config/nvim/lua/trajanoreuter/plugins/treesitter.lua
+++ .config/nvim/lua/trajanoreuter/plugins/treesitter.lua
@@ -10,16 +10,19 @@ return {
     local treesitter = require("nvim-treesitter.configs")
 
     -- configure treesitter
+    ---@diagnostic disable-next-line: missing-fields
     treesitter.setup({ -- enable syntax highlighting
       highlight = {
         enable = true,
       },
+      textobjects = { enable = true },
       -- enable indentation
       indent = { enable = false },
       -- enable autotagging (w/ nvim-ts-autotag plugin)
       autotag = {
         enable = true,
       },
+      sync_install = false,
       auto_install = true,
       -- ensure these language parsers are installed
       ensure_installed = "all",
