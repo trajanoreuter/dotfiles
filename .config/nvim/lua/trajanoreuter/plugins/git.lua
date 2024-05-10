a51e95227b8f1a6cc7e4c21cfe4b5da6bc5ec5af
Trajano Reuter <trajanoreuter@gmail.com>
2024-05-10 15:03:06 -0300

    feat: add copilot plugin

--- .config/nvim/lua/trajanoreuter/plugins/git.lua
+++ .config/nvim/lua/trajanoreuter/plugins/git.lua
@@ -110,6 +110,17 @@ return {
     end,
   },
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+    },
+  },
+  {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
