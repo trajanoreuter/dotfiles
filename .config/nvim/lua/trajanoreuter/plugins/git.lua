6613637dcf55b26b5203f3d9ad558ad8baab2ccf
Trajano Reuter <trajanoreuter@gmail.com>
2024-06-13 16:59:28 -0300

    feat: improve cmp

--- .config/nvim/lua/trajanoreuter/plugins/git.lua
+++ .config/nvim/lua/trajanoreuter/plugins/git.lua
@@ -19,6 +19,21 @@ return {
     end,
   },
   {
+    "zbirenbaum/copilot-cmp",
+    dependencies = {
+      "zbirenbaum/copilot.lua",
+    },
+    config = function()
+      require("copilot_cmp").setup()
+      require("copilot").setup({
+        suggestion = {
+          enabled = false,
+        },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
     "kdheepak/lazygit.nvim",
     cmd = {
       "LazyGit",
--- .config/nvim/lua/trajanoreuter/plugins/nvim-cmp.lua
+++ .config/nvim/lua/trajanoreuter/plugins/nvim-cmp.lua
@@ -1,63 +1,116 @@
+---@diagnostic disable: missing-fields
+
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-path", -- source for file system paths
-    {
-      "L3MON4D3/LuaSnip",
-      -- follow latest release.
-      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-      -- install jsregexp (optional!).
-      build = "make install_jsregexp",
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.3",
+      },
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
+      "windwp/nvim-ts-autotag",
+      "windwp/nvim-autopairs",
     },
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
-    "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
-  },
-  config = function()
-    local cmp = require("cmp")
+    config = function()
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
 
-    local luasnip = require("luasnip")
+      require("nvim-autopairs").setup()
 
-    local lspkind = require("lspkind")
+      -- Integrate nvim-autopairs with cmp
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require("luasnip.loaders.from_vscode").lazy_load()
+      -- Load snippets
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-    cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,preview,noselect",
-      },
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      }),
-      -- sources for autocompletion
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
-      }),
-
-      -- configure lspkind for vs-code like pictograms in completion menu
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
+      cmp.setup({
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            require("copilot_cmp.comparators").prioritize,
+            -- Below is the default comparitor list and order for nvim-cmp
+            cmp.config.compare.offset,
+            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        completion = { completeopt = "menu,menuone,noinsert" },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+          ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- scroll up preview
+          ["<C-d>"] = cmp.mapping.scroll_docs(4), -- scroll down preview
+          ["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
+          ["<C-c>"] = cmp.mapping.abort(), -- close completion window
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- select suggestion
         }),
-      },
-    })
-  end,
+        -- sources for autocompletion
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" }, -- lsp
+          { name = "buffer", max_item_count = 5 }, -- text within current buffer
+          { name = "path", max_item_count = 3 }, -- file system paths
+          { name = "luasnip", max_item_count = 3 }, -- snippets
+          { name = "copilot", group_index = 2 }, -- Copilot suggestions
+        }),
+        -- Enable pictogram icons for lsp/autocompletion
+        formatting = {
+          expandable_indicator = true,
+          format = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50,
+            ellipsis_char = "...",
+            symbol_map = {
+              Copilot = "",
+            },
+          }),
+        },
+        experimental = {
+          ghost_text = true,
+        },
+      })
+    end,
+  },
 }
