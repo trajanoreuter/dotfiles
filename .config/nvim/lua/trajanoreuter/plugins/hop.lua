63b28024fac04ff04102f29a6e2282b6209cc457
Trajano Reuter <trajanoreuter@gmail.com>
2024-06-14 11:07:40 -0300

    fix: setup dressing

--- .config/nvim/lua/trajanoreuter/plugins/hop.lua
+++ .config/nvim/lua/trajanoreuter/plugins/hop.lua
@@ -9,7 +9,6 @@ return {
 
     local directions = require("hop.hint").HintDirection
     local keymap = vim.keymap
-
     keymap.set("", "f", function()
       hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
     end, { remap = true })
@@ -17,7 +16,7 @@ return {
       hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
     end, { remap = true })
     keymap.set("", "t", function()
-      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })
     end, { remap = true })
     keymap.set("", "T", function()
       hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })
